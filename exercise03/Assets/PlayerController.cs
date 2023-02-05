13e13614d9d599cf00bf74e5ec5052d996740a99
MBMesfin <120344206+MBMesfin@users.noreply.github.com>
2023-02-05 18:59:03 -0500

    update

--- exercise03/Assets/PlayerController.cs
+++ exercise03/Assets/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+
+    float moveSpeed = 5f;
+    float rotateSpeed = 75f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * moveSpeed * vAxis, Space.World);
+
+        gameObject.transform.Rotate(0, rotateSpeed * Time.deltaTime * moveSpeed * hAxis, 0);
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("babyBottle"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
