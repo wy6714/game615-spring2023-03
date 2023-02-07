d5b7736e76560d192c72b1ed21d9acb049c7f113
MBMesfin <120344206+MBMesfin@users.noreply.github.com>
2023-02-07 09:29:29 -0500

    update

--- exercise03/Assets/CollectScore.cs
+++ exercise03/Assets/CollectScore.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CollectScore : MonoBehaviour
 {
 
     public int score;
-    float moveSpeed = 5f;
-    float rotateSpeed = 75f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +15,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
-
-        gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * moveSpeed * vAxis, Space.World);
-
-        gameObject.transform.Rotate(0, rotateSpeed * Time.deltaTime * hAxis, 0);
 
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
